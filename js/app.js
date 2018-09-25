42b76bff3a1462276e86e630d86a2a2448a8b2da
objectorange <objectorange@protonmail.com>
2018-09-25 01:49:14 -0400

    had to remove angularjs due to new hosting doesn't support custom error handling

--- js/app.js
+++ js/app.js
@@ -1,41 +1,58 @@
 'use strict';
 
 // angular.js main app initialization
-var app = angular.module('1M5',['ngRoute']);
+// var app = angular.module('1M5',['ngRoute']);
 
 // routes
-app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $locationProvider.html5Mode(true);
-    //$locationProvider.hashPrefix('!');
-    $routeProvider.
-            when('/',{templateUrl: 'templates/home.html', controller: 'mainController' }).
-            when('/home',{templateUrl: 'templates/home.html', controller: 'mainController' }).
-            when('/contracts',{templateUrl: 'templates/contracts.html', controller: 'mainController' }).
-            when('/did',{templateUrl: 'templates/did.html', controller: 'mainController' }).
-            when('/sensors',{templateUrl: 'templates/sensors.html', controller: 'mainController' }).
-            when('/content',{templateUrl: 'templates/content.html', controller: 'mainController' }).
-            when('/prana',{templateUrl: 'templates/prana.html', controller: 'mainController' }).
-            when('/aten',{templateUrl: 'templates/aten.html', controller: 'mainController' }).
-            when('/keyring',{templateUrl: 'templates/keyring.html', controller: 'mainController' }).
-            when('/infovault',{templateUrl: 'templates/infovault.html',controller: 'mainController'}).
-            when('/ssauthn',{templateUrl: 'templates/ssauthn.html',controller: 'mainController'}).
-            when('/repbac',{templateUrl: 'templates/repbac.html',controller: 'mainController'}).
-            when('/consensus',{templateUrl: 'templates/consensus.html',controller: 'mainController'}).
-            when('/dex',{templateUrl: 'templates/dex.html',controller: 'mainController'}).
-            otherwise({redirectTo: '/'})
-    }]);
+// app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+//     $locationProvider.html5Mode(true);
+//     //$locationProvider.hashPrefix('!');
+//     $routeProvider.
+//             when('/',{templateUrl: 'templates/home.html', controller: 'mainController' }).
+//             when('/home',{templateUrl: 'templates/home.html', controller: 'mainController' }).
+//             when('/contracts',{templateUrl: 'templates/contracts.html', controller: 'mainController' }).
+//             when('/did',{templateUrl: 'templates/did.html', controller: 'mainController' }).
+//             when('/sensors',{templateUrl: 'templates/sensors.html', controller: 'mainController' }).
+//             when('/content',{templateUrl: 'templates/content.html', controller: 'mainController' }).
+//             when('/prana',{templateUrl: 'templates/prana.html', controller: 'mainController' }).
+//             when('/aten',{templateUrl: 'templates/aten.html', controller: 'mainController' }).
+//             when('/keyring',{templateUrl: 'templates/keyring.html', controller: 'mainController' }).
+//             when('/infovault',{templateUrl: 'templates/infovault.html',controller: 'mainController'}).
+//             when('/ssauthn',{templateUrl: 'templates/ssauthn.html',controller: 'mainController'}).
+//             when('/repbac',{templateUrl: 'templates/repbac.html',controller: 'mainController'}).
+//             when('/consensus',{templateUrl: 'templates/consensus.html',controller: 'mainController'}).
+//             when('/dex',{templateUrl: 'templates/dex.html',controller: 'mainController'}).
+//             otherwise({redirectTo: '/'})
+//     }]);
 
 // controllers
-app.controller('mainController', ['$scope', '$http', '$location', function ($scope, $http, $location) {
-    $scope.currentPath = $location.path();
-    $scope.selectClass = function (){
-        if($location.path() === '/' || $location.path() === '/home') {
-            return "homepage";
-        } else {
-            return "no-sidebar";
-        }
-    };
-    $scope.showLogo = function(){
-        return $location.path() === '/' || $location.path() === '/home';
+// app.controller('mainController', ['$scope', '$http', '$location', function ($scope, $http, $location) {
+//     $scope.currentPath = $location.path();
+//     $scope.selectClass = function (){
+//         if($location.path() === '/') {
+//             return "homepage";
+//         } else {
+//             return "no-sidebar";
+//         }
+//     };
+//     $scope.showLogo = function(){
+//         return $location.path() === '/';
+//     }
+// }]);
+
+// $scope.currentPath = $location.path();
+
+function selectClass(){
+    if($location.path() === '/') {
+        return "homepage";
+    } else {
+        return "no-sidebar";
     }
-}]);
+}
+
+var l = document.getElementById("logo");
+if ($location.path() === '/') {
+    l.style.display = "block";
+} else {
+    l.style.display = "none";
+}
